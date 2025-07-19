d7f5327e7d46d9385fe2ac56a5bfeacebfc43b36
Mohd Aman <mohdaman1018@gmail.com>
2025-07-19 13:13:54 +0530

    Pushing

--- src/main/java/com/example/Quiz_App/Question.java
+++ src/main/java/com/example/Quiz_App/Question.java
@@ -13,11 +13,14 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
-    private String questionTitle;
+    private String category;
+    private String difficultylevel;
     private String option1;
     private String option2;
     private String option3;
     private String option4;
+    private String questionTitle;
     private String rightAnswer;
-    private String difficultylevel;
+
+
 }
