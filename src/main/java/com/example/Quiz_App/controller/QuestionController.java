5e03c63157c56fc47a087f80ecb8cf085dba8647
Mohd Aman <mohdaman1018@gmail.com>
2025-07-19 13:50:41 +0530

    Pushing

--- src/main/java/com/example/Quiz_App/controller/QuestionController.java
+++ src/main/java/com/example/Quiz_App/controller/QuestionController.java
@@ -4,10 +4,7 @@ package com.example.Quiz_App.controller;
 import com.example.Quiz_App.Question;
 import com.example.Quiz_App.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +25,8 @@ public class QuestionController {
         return questionService.getQuestionsByCategory(category);
     }
 
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
 }
