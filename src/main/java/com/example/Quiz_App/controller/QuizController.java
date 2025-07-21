794c2e295867f04a94909d4d37e5ff98857eff08
Mohd Aman <mohdaman1018@gmail.com>
2025-07-21 14:28:29 +0530

    Pushing

---
+++ src/main/java/com/example/Quiz_App/controller/QuizController.java
@@ -0,0 +1,23 @@
+package com.example.Quiz_App.controller;
+
+import com.example.Quiz_App.service.QuizService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
+        return quizService.createQuiz(category, numQ, title);
+
+    }
+
+}
---
+++ src/main/java/com/example/Quiz_App/service/QuizService.java
@@ -0,0 +1,10 @@
+package com.example.Quiz_App.service;
+
+import org.springframework.http.ResponseEntity;
+
+public class QuizService {
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+    }
+}
